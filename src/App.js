e4f63548916ee2e914918bca9f7475bff9b8b533
douik123 <ahmed@predict-a.com>
2022-08-24 17:28:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  let [count, setCount] = useState(0);
+  function inc() {
+    setCount(count+1);
+  }
+  function dec() {
+    setCount(count - 1);
+  }
+  useEffect(() => {
+    console.log(count%10)
+    if ((count%10===0)&&(count!==0)) {
+     alert("num divisible par 10");
+  }
+},[count]);
   return (
+    <div className='all'>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        <div className='container'>
+      <button className='plus' onClick={inc}>+</button>
+      <div className='resu'>{count}</div>
+      <button className='min' onClick={dec}>-</button>
+      {
+
+      }
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
-
 export default App;
